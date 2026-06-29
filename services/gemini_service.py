1103afd66b911d006c7d0481f255996f5d92349d
Gokul Atmaram Pawar <gokulpawar636@gmail.com>
2026-06-29 21:34:53 +0530

    Update gemini_service.py

--- services/gemini_service.py
+++ services/gemini_service.py
@@ -1,16 +1,27 @@
 import google.generativeai as genai
 from config.config import GOOGLE_API_KEY
 
+print("Loading Gemini...")
+
+print("API KEY:", GOOGLE_API_KEY[:10] if GOOGLE_API_KEY else "None")
+
 genai.configure(api_key=GOOGLE_API_KEY)
 
 model = genai.GenerativeModel("gemini-2.5-flash")
 
-
 def ask_gemini(prompt):
-    print("Gemini request started")
 
-    response = model.generate_content(prompt)
+    print("Prompt length:", len(prompt))
 
-    print("Gemini response received")
+    try:
+        print("Sending request...")
 
-    return response.text
+        response = model.generate_content(prompt)
+
+        print("Response received")
+
+        return response.text
+
+    except Exception as e:
+        print("Gemini Error:", e)
+        raise
